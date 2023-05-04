8d792a48676da4885e7432d33577ff11bb3b1e13
s2 <joel_sotelo_trobat@outlook.com>
2023-05-04 12:18:06 -0300

    SEO improvements

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,26 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document () {
   return (
     <Html lang='en'>
-      <Head />
+      <Head>
+        <meta charSet='utf-8' />
+        <link rel='icon' href='/favicon.ico' />
+        <meta
+          name='description'
+          content='A clone of the Netflix homepage, built with React, Next.js, Tailwind CSS, and TypeScript.'
+        />
+        <meta name='author' content='Joel Sotelo' />
+
+        <meta property='og:title' content='Netflix clone' />
+        <meta
+          property='og:description'
+          content='A clone of the Netflix homepage, built with React, Next.js, Tailwind CSS, and TypeScript.'
+        />
+        <meta
+          property='og:image'
+          content='https://raw.githubusercontent.com/amafoas/netflix-clone/main/public/og_image.jpg'
+        />
+        <meta property='og:type' content='website' />
+      </Head>
       <body>
         <Main />
         <NextScript />
